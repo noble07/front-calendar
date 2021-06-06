7429a1e7a759e07e6bec900fc2d847763108bb42
Juan Camilo <juancmz98@gmail.com>
2021-06-06 13:05:56 -0500

    Se crea la acción para hacer el dispatch del login

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,17 +1,39 @@
+import { useDispatch } from 'react-redux';
+import { useForm } from 'hooks/useForm';
+import { startLogin } from 'actions/auth';
+
 import './login.css';
 
 const LoginScreen = () => {
+
+    const dispatch = useDispatch()
+    const [formLoginValues, handleLoginInputChange] = useForm({
+        lEmail: 'juan@gmail.com',
+        lPassword: '123456',
+    })
+
+    const {lEmail, lPassword} = formLoginValues
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+
+        dispatch(startLogin(lEmail, lPassword))
+    }
+
     return (
         <div className="container login-container">
             <div className="row">
                 <div className="col-md-6 login-form-1">
                     <h3>Ingreso</h3>
-                    <form>
+                    <form onSubmit={handleLogin} >
                         <div className="mb-3">
                             <input 
                                 type="text"
                                 className="form-control"
                                 placeholder="Correo"
+                                name="lEmail"
+                                value={lEmail}
+                                onChange={handleLoginInputChange}
                             />
                         </div>
                         <div className="mb-3">
@@ -19,6 +41,9 @@ const LoginScreen = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña"
+                                name="lPassword"
+                                value={lPassword}
+                                onChange={handleLoginInputChange}
                             />
                         </div>
                         <div className="mb-3">
