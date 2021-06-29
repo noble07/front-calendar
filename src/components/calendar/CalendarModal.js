1ffb46810637c34260c43baa0606bc48be839bfe
Juan Camilo <juancmz98@gmail.com>
2021-06-28 23:17:08 -0500

    Pruebas en AppRouter

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -19,8 +19,7 @@ const customStyles = {
     transform: 'translate(-50%, -50%)'
   }
 }
-
-Modal.setAppElement('#root')
+if (process.env.NODE_ENV !== 'test') Modal.setAppElement('#root')
 
 const now = moment().minute(0).seconds(0).add(1, 'hours')
 const nowPlusOne = now.clone().add(1, 'hours')
